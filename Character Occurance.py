032c7d1a07c4011405e4b0619a4ea6212fc1f293
Majd833 <lina.azzam.1611@gmail.com>
2024-10-01 14:08:00 +0300

    Character occurance

---
+++ Character Occurance.py
@@ -0,0 +1,9 @@
+string=str(input("Enter a string:"))
+char=input("Enter a character:")
+i=0
+count=0
+while i<len(string):
+    if (string[i]==char):
+        count=count+1
+    i=i+1
+print("The character",char,"has occured",count,"times")    
